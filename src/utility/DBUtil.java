830471ed568c493657c3079ededbd31e465a0626
shishir1997 <103549258+shishir1997@users.noreply.github.com>
2022-11-12 14:26:24 +0530

    add

--- src/utility/DBUtil.java
+++ src/utility/DBUtil.java
@@ -1,5 +1,33 @@
 package utility;
 
-public class DBUtil {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil { 
+	
+	public static Connection provideConnection() {
+
+		Connection connec = null;
+
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			connec =	DriverManager.getConnection("jdbc:mysql://localhost:3306/fw19sb101","root","root");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+
+
+		return connec;
+	}
+	
 
 }
